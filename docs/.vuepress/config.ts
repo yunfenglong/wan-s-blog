0006918e58c64e833f5606b6135ba1885b03e231
wanfunglung <yunfeng.long@yahoo.com>
2025-08-01 18:24:27 +0800

    Update deploy base URL

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/',
+  base: '/wan-s-blog/',
   lang: 'en-US',
   locales: {
     '/': {
